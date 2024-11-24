197fc8ca0383e9209c5a1d50a310a00057544e96
wengmister <wengmister@gmail.com>
2024-11-24 13:15:34 -0600

    Initializing package

---
+++ src/apex_putter/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'apex_putter'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Zhengyang Kris Weng',
+    maintainer_email='wengmister@gmail.com',
+    description='ME495 Final Project Fall 2024 Group 3 - Apex Putter: a mini-golf robot',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
