2be3ebd1cb643ffa158e5b71ade7be4f9babfc6b
wengmister <wengmister@gmail.com>
2024-11-25 15:57:14 -0600

    Apriltag now working

--- src/apex_putter/setup.py
+++ src/apex_putter/setup.py
@@ -11,7 +11,8 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch/', ['launch/vision.launch.xml']),
-        ('share/' + package_name + '/config/', ['config/apriltag.rviz'])
+        ('share/' + package_name + '/config/', ['config/apriltag.rviz',
+                                                'config/atag_ros.yaml'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
