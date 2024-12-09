8d59bc8c573ceb0bc66cc3872f3278ad5f7d83be
sayantani <sayantanibhattacharya2025@u.northwestern.edu>
2024-12-08 23:27:05 -0600

    Added calculations for swing pose and orientations.

--- src/apex_putter/apex_putter/demo.py
+++ src/apex_putter/apex_putter/demo.py
@@ -4,7 +4,7 @@ from rclpy.node import Node
 from geometry_msgs.msg import Pose, Quaternion
 from std_srvs.srv import Empty
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
-
+import math
 import tf2_ros
 
 from apex_putter.MotionPlanningInterface import MotionPlanningInterface
@@ -34,6 +34,16 @@ class DemoNode(Node):
         # Known offsets:
         self.putter_length = 22.85 * 0.0254  # About 0.58 m
         self.putter_offset = 0.18 * 0.0254  # About 0.00457 m
+
+        # Dimentions:
+        self.puttface_dim = [0,0,0]
+        self.goal_ball_radius = 2.03
+        # testing vals.
+        self.putface_ee_transform =  np.array([
+                                                [0.7071, -0.7071, 0, 1],
+                                                [0.7071, 0.7071, 0, 0.44454056],
+                                                [0, 0, 1, 0.66401457],
+                                                [0, 0, 0, 1]])
 
         # Motion planning interface
         self.MPI = MotionPlanningInterface(
@@ -95,26 +105,77 @@ class DemoNode(Node):
         self.look_up_hole_in_base_frame()
         self.hole_position[2] = self.ball_position[2] # flatten the hole position on z-axis
         return self.ball_position - self.hole_position # vector: hole to ball
+
+    def calculate_ball_to_puttface_vector(self, v_h2b, putting_distance = 0.1):
+        '''
+            Calculate the vector from the centre of ball to the centre of puttface.
+
+            Args: 
+                v_h2b = vector from hole to ball.
+                putting_distance (Optional) : the distance in direction of 
+                                    putting where the robot would be positioned to start the swing action.
+
+            Returns: The pose of the putterface to start the swing.
+        '''        
+        dx = v_h2b[0]
+        dy = v_h2b[1]
+        dz = v_h2b[2]
+        distance = math.sqrt(dx**2 + dy**2 + dz**2)
+        scaling_factor = (putting_distance)/ distance         
+        # Coordinates of the ball center
+        x_r = dx * scaling_factor + self.puttface_dim[0]
+        y_r = dy * scaling_factor + self.puttface_dim[1]
+        z_r = dz * scaling_factor + self.puttface_dim[2]
+        v_b2p = [x_r,y_r,z_r]
+        return v_b2p
+    
+    def calculate_putting_orientation(self, v_b2p):
+        """ Caculating the unit vector (Quaternion) in the direction opposite to ball to puttface vector."""
         
+        norm_ang = np.linalg.norm(v_b2p)
+        if norm_ang == 0:
+            raise ValueError("Ball to putter vector has no direction.")
+        unit_vector = v_b2p / norm_ang
+        # Reverse direction
+        opposite_vector = -unit_vector
+        putting_angle = -norm_ang
+
+        # Calculate quaternion components : need confirm the formula.
+        w = np.cos(putting_angle / 2)
+        x = unit_vector[0] * np.sin(putting_angle / 2)
+        y = unit_vector[1] * np.sin(putting_angle / 2)
+        z = unit_vector[2] * np.sin(putting_angle / 2)
+        orientation = Quaternion(x=x, y=y, z=z, w=w)
+        return orientation
+    
+
     async def ready_callback(self, request, response):
         """Prepare the robot for putting"""
         self.get_logger().info("Ready requested.")
         self.get_logger().info("=============================================================")
 
         self.v_h2b = self.calculate_hole_to_ball_vector()
+        self.v_b2p = self.calculate_ball_to_puttface_vector(self.v_h2b)   
+        pose_wrt_putface = np.zeros(3)
+        pose_wrt_putface[0] = self.ball_position[0] + self.v_b2p[0]
+        pose_wrt_putface[1] = self.ball_position[1] + self.v_b2p[1]
+        pose_wrt_putface[2]= self.ball_position[2] + self.v_b2p[2]
 
-        self.offset_ball_position(0.60)
+        # Puttface to ee transform
+        pose_wrt_putface_homogeneous = np.append(pose_wrt_putface, 1).reshape(1, 4)
+        pose_wrt_ee = pose_wrt_putface_homogeneous * self.putface_ee_transform
+
+        orientation = self.calculate_putting_orientation(self.v_b2p)
         ball_pose = Pose()
-        ball_pose.position.x = self.ball_position[0] + 0.1 * self.v_h2b[0]
-        ball_pose.position.y = self.ball_position[1] + 0.1 * self.v_h2b[1]
-        ball_pose.position.z = self.ball_position[2]
-        # make oritentation vertical downwards
-        ball_pose.orientation = Quaternion(x=0.92, y=-0.38, z=0.00035, w=0.0004)
+        ball_pose.position.x = pose_wrt_ee[0]
+        ball_pose.position.y = pose_wrt_ee[1]
+        ball_pose.position.z = pose_wrt_ee[2]
+        ball_pose.orientation = orientation
         await self.MPI.move_arm_pose(ball_pose, max_velocity_scaling_factor=0.5, max_acceleration_scaling_factor=0.5)
         return response
     
     async def putt_callback(self, request, response):
-        """Putt the fucking ball"""
+        """Putt the ball"""
         self.get_logger().info("Putt requested.")
         self.get_logger().info("=============================================================")
 
