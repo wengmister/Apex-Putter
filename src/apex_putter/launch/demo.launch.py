714767719c938900af76ee15ffe9d3f248347658
wengmister <wengmister@gmail.com>
2024-12-09 16:19:02 -0600

    Fixed demo launch file

--- src/apex_putter/launch/demo.launch.py
+++ src/apex_putter/launch/demo.launch.py
@@ -1,32 +1,31 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.conditions import IfCondition
-from launch.substitutions import EqualsSubstitution, LaunchConfiguration, \
-    PathJoinSubstitution
+from launch.substitutions import EqualsSubstitution, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from moveit_configs_utils import MoveItConfigsBuilder
 
-
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder(
-        'fer', package_name='franka_fer_moveit_config').to_moveit_configs()
-
     return LaunchDescription([
-        DeclareLaunchArgument('demo', default_value='true',
-                              description='Sets the mode to demo.'),
         IncludeLaunchDescription(
-            launch_description_source=[FindPackageShare(
-                'franka_fer_moveit_config'), '/launch', '/franka_fer_moveit_config'],
-            launch_arguments={'robot_ip': 'panda0.robot'}  
+            launch_description_source=PathJoinSubstitution([
+                FindPackageShare('franka_fer_moveit_config'),
+                'launch',
+                'moveit_rviz.launch.py'
+            ]),
+            launch_arguments={'robot_ip': 'panda0.robot'}.items()
         ),
         IncludeLaunchDescription(
-            launch_description_source=[FindPackageShare(
-                'apex_putter'), '/launch', '/vision.launch.xml']
+            launch_description_source=PathJoinSubstitution([
+                FindPackageShare('apex_putter'),
+                'launch',
+                'vision.launch.xml'
+            ])
         ),
         Node(
             package='apex_putter',
             executable='demo',
-            exec_name='demo'
+            name='demo'  # Changed from exec_name to name
         )
     ])
